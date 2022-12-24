f4fdac02a3a4dc2f92a236fe93af6926a9a921dc
ReySnow <1286653145@qq.com>
2022-12-24 16:29:53 +0800

    demo

---
+++ demo/react.js
@@ -0,0 +1,12 @@
+import { useReducer, Component } from "react";
+import ReactDOM from "react-dom";
+
+// import { useReducer } from "../src/react";
+// import { Component } from "../src/react";
+// import ReactDOM from "../src/react-dom";
+
+export {
+    ReactDOM,
+    Component,
+    // useReducer
+};
---
+++ demo/src/main.jsx
@@ -0,0 +1,53 @@
+import { ReactDOM, Component } from "../react";
+
+import "./index.css";
+
+function FunctionComponent(props) {
+    return (
+        <div className="border">
+            <p>{props.name}</p>
+        </div>
+    );
+}
+
+class ClassComponent extends Component {
+    render() {
+        return (
+            <div className="border">
+                <h3>{this.props.name}</h3>
+                我是文本
+            </div>
+        );
+    }
+}
+
+function FragmentComponent() {
+    return (
+        <ul>
+            <>
+                <li>part1</li>
+                <li>part2</li>
+            </>
+        </ul>
+    );
+}
+
+const jsx = (
+    <div className="border">
+        <h1>react</h1>
+        <a href="https://github.com/bubucuo/mini-react">mini react</a>
+        <FunctionComponent name="函数组件" />
+        <ClassComponent name="类组件" />
+        <FragmentComponent />
+    </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(jsx);
+
+// 实现了常见组件初次渲染
+
+// 原生标签
+// 函数组件
+// 类组件
+// 文本
+// Fragment
